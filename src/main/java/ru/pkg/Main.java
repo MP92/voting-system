b0ed0004deec620b0c4f1c4d8891c8f6b0765d06
MP92 <max.nn92@gmail.com>
2016-05-23 15:37:45 +0300

    added logging

--- src/main/java/ru/pkg/Main.java
+++ src/main/java/ru/pkg/Main.java
@@ -1,7 +1,12 @@
 package ru.pkg;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        logger.debug("hello from Main!");
     }
 }
