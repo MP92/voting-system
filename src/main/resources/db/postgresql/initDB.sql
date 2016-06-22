7d3a884bae1fce2f2fab4678479f4a54aa876649
MP92 <max.nn92@gmail.com>
2016-06-22 11:06:15 +0300

    Fix initDB.sql

--- src/main/resources/db/postgresql/initDB.sql
+++ src/main/resources/db/postgresql/initDB.sql
@@ -15,13 +15,13 @@ CREATE TABLE users (
   name VARCHAR NOT NULL,
   surname VARCHAR NOT NULL,
   password VARCHAR NOT NULL,
-  registered TIMESTAMP DEFAULT now(),
-  enabled BOOLEAN DEFAULT TRUE
+  registered TIMESTAMP NOT NULL DEFAULT now(),
+  enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 CREATE UNIQUE INDEX users_unique_name_idx ON users(name, surname);
 
 CREATE TABLE roles (
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   role VARCHAR NOT NULL,
   UNIQUE(user_id, role)
 );
@@ -29,33 +29,32 @@ CREATE TABLE roles (
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
-  description VARCHAR NULL,
+  description VARCHAR NOT NULL,
   address VARCHAR NOT NULL,
   phone_number VARCHAR NOT NULL
 );
 CREATE UNIQUE INDEX restaurant_unique_name_idx ON restaurants(name);
 
 CREATE TABLE dishes (
-  id SERIAL,
-  restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
+  id SERIAL PRIMARY KEY,
+  restaurant_id INTEGER NOT NULL REFERENCES restaurants(id) ON DELETE CASCADE,
   name VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
   weight INTEGER NOT NULL,
   category VARCHAR NOT NULL,
   price NUMERIC(7, 2) NOT NULL,
-  in_menu BOOLEAN DEFAULT FALSE,
-  PRIMARY KEY (id, restaurant_id),
-  UNIQUE(restaurant_id, name)
+  in_menu BOOLEAN NOT NULL DEFAULT FALSE
 );
+CREATE UNIQUE INDEX dishes_unique_name_idx ON dishes(restaurant_id, name);
 
 CREATE TABLE votes (
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
-  last_voted TIMESTAMP NOT NULL DEFAULT now(),
+  last_voted TIMESTAMP DEFAULT now(),
   UNIQUE(user_id, restaurant_id)
 );
 
 CREATE TABLE voting_statistics (
-  restaurant_id INTEGER UNIQUE REFERENCES restaurants(id) ON DELETE CASCADE,
+  restaurant_id INTEGER NOT NULL UNIQUE REFERENCES restaurants(id) ON DELETE CASCADE,
   votes INTEGER NOT NULL
 );
